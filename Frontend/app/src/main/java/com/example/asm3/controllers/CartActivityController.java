eb0710675ab5128f7bd8d5a9a4090f837625fd49
haihoang1903 <s3818558@rmit.edu.vn>
2023-01-18 15:18:26 +0700

    update cart

--- Frontend/app/src/main/java/com/example/asm3/controllers/CartActivityController.java
+++ Frontend/app/src/main/java/com/example/asm3/controllers/CartActivityController.java
@@ -1,37 +1,76 @@
 package com.example.asm3.controllers;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.asm3.R;
+import com.example.asm3.base.adapter.GenericAdapter;
+import com.example.asm3.base.adapter.viewHolder.OrderHolder;
 import com.example.asm3.base.controller.BaseController;
 import com.example.asm3.base.networking.services.AsyncTaskCallBack;
 import com.example.asm3.custom.components.TopBarView;
+import com.example.asm3.models.Book;
+import com.example.asm3.models.CartItem;
+import com.example.asm3.models.OrderDetail;
+
+import java.util.ArrayList;
 
 public class CartActivityController extends BaseController implements
         AsyncTaskCallBack,
+        OrderHolder.OnSelectListener,
         View.OnClickListener {
     private TopBarView topBar;
     private RecyclerView cartRecView;
-    private Button backBtn;
+    private TextView totalOrderTxt;
+    private Button backBtn, checkoutBtn;
+    private ArrayList<CartItem> cartItems;
+    private GenericAdapter<CartItem> cartItemsAdapter;
+    private float totalPrice;
+
 
     public CartActivityController(Context context, FragmentActivity activity) {
         super(context, activity);
         topBar = getActivity().findViewById(R.id.cartTopBar);
         topBar.setSubPage("Your Cart");
+        cartItems = new ArrayList<>();
+
     }
 
     @Override
     public void onInit() {
         backBtn = topBar.getBackButton();
         backBtn.setOnClickListener(this);
-    }
 
+        cartItems.add(new CartItem(2, new Book("Lord of the ring", "J. R. R. Tolkien", null, 100000, 0, null, null, null, null, null, true, null)));
+        cartItems.add(new CartItem(2, new Book("Lord of the ring", "J. R. R. Tolkien", null, 100000, 0, null, null, null, null, null, true, null)));
+        cartItems.add(new CartItem(2, new Book("Lord of the ring", "J. R. R. Tolkien", null, 100000, 0, null, null, null, null, null, true, null)));
+        cartItems.add(new CartItem(2, new Book("Lord of the ring", "J. R. R. Tolkien", null, 100000, 0, null, null, null, null, null, true, null)));
+        cartItems.add(new CartItem(2, new Book("Lord of the ring", "J. R. R. Tolkien", null, 100000, 0, null, null, null, null, null, true, null)));
+        cartItems.add(new CartItem(2, new Book("Lord of the ring", "J. R. R. Tolkien", null, 100000, 0, null, null, null, null, null, true, null)));
+
+        cartRecView = getActivity().findViewById(R.id.cartRecView);
+        cartItemsAdapter = generateOrderAdapter();
+        loadPurchased();
+
+        totalOrderTxt = getActivity().findViewById(R.id.totalOrderTxt);
+        for (int i=0; i< cartItems.size();i++){
+            totalPrice = totalPrice + cartItems.get(i).getProduct().getPrice()*cartItems.get(i).getQuantity();
+
+        }
+        totalOrderTxt.setText("Total: "+ totalPrice);
+    }
 
     @Override
     public void onClick(View view) {
@@ -40,6 +79,36 @@ public class CartActivityController extends BaseController implements
                 getActivity().finish();
                 break;
         }
+    }
+
+    @Override
+    public void onOrderClick(int position, View view) {
+        cartItems.remove(position);
+        cartItemsAdapter.notifyItemRemoved(position);
+    }
+
+    private GenericAdapter<CartItem> generateOrderAdapter() {
+        return new GenericAdapter<CartItem>(cartItems) {
+            @Override
+            public RecyclerView.ViewHolder setViewHolder(ViewGroup parent) {
+                final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_item, parent, false);
+                return new OrderHolder(view, CartActivityController.this);
+            }
+
+            @Override
+            public void onBindData(RecyclerView.ViewHolder holder, CartItem cartItem) {
+                OrderHolder orderHolder = (OrderHolder) holder;
+                orderHolder.getOrderBookTxt().setText(cartItem.getProduct().getName());
+                orderHolder.getOrderQuantityTxt().setText("Quantity: " + cartItem.getQuantity());
+                orderHolder.getOrderPriceTxt().setText(cartItem.getProduct().getPrice()*cartItem.getQuantity() + " đ");
+            }
+        };
+    }
+
+    private void loadPurchased() {
+        cartRecView.setLayoutManager(new LinearLayoutManager(getContext()));
+        cartRecView.setAdapter(cartItemsAdapter);
+
     }
 
     @Override
