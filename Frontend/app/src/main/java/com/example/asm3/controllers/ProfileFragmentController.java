c44b9ecc11805e6afb79f704eccad0264c97b04e
haihoang1903 <s3818558@rmit.edu.vn>
2023-01-20 18:47:31 +0700

    get review

--- Frontend/app/src/main/java/com/example/asm3/controllers/ProfileFragmentController.java
+++ Frontend/app/src/main/java/com/example/asm3/controllers/ProfileFragmentController.java
@@ -596,6 +596,7 @@ public class ProfileFragmentController extends BaseController implements
         } else if (taskType.equals(Constant.uploadReviewTaskType)) {
 
         } else if (taskType.equals(Constant.getAllCustomerReviewsTaskType)) {
+            Log.d("dada","jojojo");
             ApiList<Review> apiList = ApiList.fromJSON(ApiList.getData(message), Review.class);
             mainViewModel.setReviews(apiList.getList());
         }
